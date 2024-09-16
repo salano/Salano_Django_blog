9b5a3f9168bab6c02e371176e094636e03d71f43
Cleveland Sullivan <salanosullivan@gmail.com>
2024-09-17 00:55:31 +0200

    Add login and profile page

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,3 +128,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
