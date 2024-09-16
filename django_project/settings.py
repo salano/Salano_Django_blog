71d1317d2ec8c201078dac30c191b56ce8262439
Cleveland Sullivan <salanosullivan@gmail.com>
2024-09-16 19:15:23 +0200

    Add registration page

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'salano_blog.apps.SalanoBlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Setting bootstrap version for installed crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- salano_blog/views.py
+++ salano_blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-post = [
-    {
-        'author': 'SalanoCS',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'September 14, 2024'
-    },
-    {
-        'author': 'TanisTS',
-        'title': 'Blog post 2',
-        'content': 'second post content',
-        'date_posted': 'September 14, 2024'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': post
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
